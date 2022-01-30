4943db74ba7eb048310e2b5425fd747f238e329f
Ved Panse <vedkpanse@gmail.com>
2022-01-30 16:55:18 +0530

    intelliJ

--- Flask/Paradigm Shift/server.py
+++ Flask/Paradigm Shift/server.py
@@ -2,10 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global link_text
+    global links
+    link_text = []
+    links = []
+    query = request.form['query']
 
-@app.route("/")
-def host():
-    return render_template("index.html")
+    url = 'https://google.com/search?q=' + query
+    request_result = requests.get(url)
+
+    soup = bs4.BeautifulSoup(request_result.text,
+                             "html.parser")
+
+    heading_object = soup.find_all('h3')
+
 
 
 if __name__ == "__main__":
--- Selenium/Cookies/main.py
+++ Selenium/Cookies/main.py
@@ -2,7 +2,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-chrome_driver_path = "/Users/vedpanse/Desktop/Develope/chromedriver 3"
+chrome_driver_path = "/Users/vedpanse/Desktop/Develope/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 URL = "https://orteil.dashnet.org/experiments/cookie/"
